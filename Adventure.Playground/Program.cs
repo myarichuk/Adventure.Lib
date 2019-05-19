4ebc3a7da4692417a041162a5358abb6c8f111f3
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-05-19 12:22:33 +0300

    WIP

--- Adventure.Playground/Program.cs
+++ Adventure.Playground/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
+using Adventure.Map;
 
 namespace Adventure.Playground
 {
     internal static class Program
     {
+        public struct Foo
+        {
+            public int X;
+            public long Y;
+            public double Z;
+
+            public override string ToString()
+            {
+                return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}, {nameof(Z)}: {Z}";
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var map = new Map<Foo>("AA",10,10);
+            map.Put(1,1,new Foo{ X = 1, Y = 123, Z = 4.56 });
         }
     }
 }
---
+++ Adventure.World/PlateGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adventure.World
+{
+    public class PlateGenerator
+    {
+        
+    }
+}
