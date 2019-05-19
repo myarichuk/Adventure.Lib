f8c02680415db8ca4ddfbcc36f4b16eb3d53b708
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-05-19 08:21:04 +0300

    basic project structure

---
+++ Adventure.Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adventure.Playground
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
